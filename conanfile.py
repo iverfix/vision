0f3480689558313b54d1fb80fc85db20e3235982
Iver Andreas Ugelvik <iver.andreas.ugelvik@hotmail.com>
2026-07-05 09:08:53 +0000

    Added conan file

---
+++ conanfile.py
@@ -0,0 +1,36 @@
+import os
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain
+
+class ConanConfig(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeDeps"  
+
+    def requirements(self):
+        self.requires("eigen/5.0.1", force=True)
+        self.requires("opencv/4.13.0")
+
+    def configure(self):
+        self.options["opencv"].with_cuda = False
+        self.options["opencv"].with_qt = False
+        self.options["opencv"].with_gtk = False
+        self.options["opencv"].with_vtk = False
+        self.options["opencv"].with_openexr = False
+        self.options["opencv"].with_ffmpeg = True  
+
+        self.options["ffmpeg"].with_pulse = False
+        self.options["ffmpeg"].with_vaapi = False
+        self.options["ffmpeg"].with_vdpau = False
+        self.options["ffmpeg"].with_xcb = False
+        self.options["ffmpeg"].with_xlib = False
+        self.options["ffmpeg"].with_vulkan = False
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        
+        if "pulseaudio" in self.dependencies:
+            pulse_path = os.path.join(self.dependencies["pulseaudio"].package_folder, "lib", "pkgconfig")
+            tc.variables["PKG_CONFIG_PATH"] = pulse_path
+            
+        tc.generate()
+
