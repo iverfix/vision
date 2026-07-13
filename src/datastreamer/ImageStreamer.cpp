4cb93188780a82cbbca8ade70c913123f9044426
Iver Andreas Ugelvik <iver.andreas.ugelvik@hotmail.com>
2026-07-13 11:26:30 +0000

    Pruned whitespace

--- src/datastreamer/ImageStreamer.cpp
+++ src/datastreamer/ImageStreamer.cpp
@@ -10,7 +10,7 @@ ImageStreamer::ImageStreamer(std::filesystem::path path) : dataset_path(std::mov
 
 void ImageStreamer::fetchDataset()
 {
-  const auto files = fetchDatasetFiles(dataset_path);
+  const std::vector<std::filesystem::path> files = fetchDatasetFiles(dataset_path);
   const std::vector<TimestampType> timestamps = fetchTimestamps(dataset_path);
 
   for (auto &&[time, file] : std::views::zip(timestamps, files)) {
@@ -26,7 +26,6 @@ std::optional<ImageData> ImageStreamer::fetchNext()
   if (nextImageIndex >= maxImageIndex) { return std::nullopt; }
 
   const auto currentImageMetadata = imageFiles[nextImageIndex];
-
   const auto imageData = cv::imread(dataset_path / "data" / currentImageMetadata.path, cv::IMREAD_GRAYSCALE);
 
   ++nextImageIndex;
